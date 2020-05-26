afbedc84c88f713bdf20212d69686be71bf2e840
Sorin Gabriel Lupu <lupu_sg@yahoo.com>
2020-05-26 22:38:31 +0300

    Update unsplash.js

--- src/api/unsplash.js
+++ src/api/unsplash.js
@@ -4,6 +4,6 @@ export default axios.create({
   baseURL: "https://api.unsplash.com",
   headers: {
     Authorization:
-      "Client-ID 39a537fa9239332d10a213820f904b90f51e48bf8ac32b9cc3515adc7fda9732"
+      "Client-ID something"
   }
 });
